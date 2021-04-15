705793fc274d89795bbf5d3c9f3a58902ef3773a
Pablo Yañez Martin <pabloya.97@gmail.com>
2021-04-15 12:29:49 +0200

    Subiendo tareas

---
+++ Mandatory task. Module 10/src/covid.ts
@@ -0,0 +1,151 @@
+export interface resultado {
+  name: string;
+  value: number;
+}
+
+export const covidBefore: resultado[] = [
+  {
+    name: "Comunidad de Madrid",
+    value: 174,
+  },
+  {
+    name: "La Rioja",
+    value: 39,
+  },
+  {
+    name: "Andalucía",
+    value: 34,
+  },
+  {
+    name: "Cataluña",
+    value: 24,
+  },
+  {
+    name: "Comunidad Valenciana",
+    value: 30,
+  },
+  {
+    name: "Región de Murcia",
+    value: 0,
+  },
+  {
+    name: "Extremadura",
+    value: 6,
+  },
+  {
+    name: "Castilla-La Mancha",
+    value: 16,
+  },
+  {
+    name: "País Vasco",
+    value: 45,
+  },
+  {
+    name: "Cantabria",
+    value: 10,
+  },
+  {
+    name: "Principado de Asturias",
+    value: 5,
+  },
+  {
+    name: "Galicia",
+    value: 3,
+  },
+  {
+    name: "Aragón",
+    value: 11,
+  },
+  {
+    name: "Castilla y León",
+    value: 19,
+  },
+  {
+    name: "Islas Canarias",
+    value: 18,
+  },
+  {
+    name: "Islas Baleares",
+    value: 6,
+  },
+  {
+    name: "Comunidad Foral de Navarra",
+    value: 69,
+  },
+  {name: "Ceuta y Melilla",
+  value:  20,
+  },
+];
+
+export const covidAfter: resultado[] = [
+  {
+    name: "Comunidad de Madrid",
+    value: 324,
+  },
+  {
+    name: "La Rioja",
+    value: 202,
+  },
+  {
+    name: "Andalucía",
+    value: 190,
+  },
+  {
+    name: "Cataluña",
+    value: 199,
+  },
+  {
+    name: "Comunidad Valenciana",
+    value: 35,
+  },
+  {
+    name: "Región de Murcia",
+    value: 68,
+  },
+  {
+    name: "Extremadura",
+    value: 132,
+  },
+  {
+    name: "Castilla-La Mancha",
+    value: 143,
+  },
+  {
+    name: "País Vasco",
+    value: 295,
+  },
+  {
+    name: "Cantabria",
+    value: 144,
+  },
+  {
+    name: "Principado de Asturias",
+    value: 163,
+  },
+  {
+    name: "Galicia",
+    value: 70,
+  },
+  {
+    name: "Aragón",
+    value: 202,
+  },
+  {
+    name: "Castilla y León",
+    value: 187,
+  },
+  {
+    name: "Islas Canarias",
+    value: 126,
+  },
+  {
+    name: "Islas Baleares",
+    value: 66,
+  },
+  {
+    name: "Comunidad Foral de Navarra",
+    value: 395,
+  },
+  {name: "Ceuta y Melilla",
+  value:  200},
+];
