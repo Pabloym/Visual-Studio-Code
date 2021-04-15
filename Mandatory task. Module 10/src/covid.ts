1b81bb73c44c61b969b11e75e99900846dbd7e4a
Pablo Yañez Martin <pabloya.97@gmail.com>
2021-04-15 12:37:32 +0200

    subiendo tarea

--- Mandatory task. Module 10/src/covid.ts
+++ Mandatory task. Module 10/src/covid.ts
@@ -3,81 +3,241 @@ export interface resultado {
   value: number;
 }
 
-export const covidBefore: resultado[] = [
-  {
-    name: "Comunidad de Madrid",
-    value: 174,
-  },
-  {
-    name: "La Rioja",
-    value: 39,
-  },
+// IA 14 dias (20/03/2020) link datos:https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Actualizacion_50_COVID-19.pdf
+
+export const ola1: resultado[] = [
   {
     name: "Andalucía",
-    value: 34,
-  },
-  {
-    name: "Cataluña",
-    value: 24,
-  },
-  {
-    name: "Comunidad Valenciana",
-    value: 30,
-  },
-  {
-    name: "Región de Murcia",
-    value: 0,
-  },
-  {
-    name: "Extremadura",
-    value: 6,
-  },
-  {
-    name: "Castilla-La Mancha",
-    value: 16,
-  },
-  {
-    name: "País Vasco",
-    value: 45,
-  },
-  {
-    name: "Cantabria",
-    value: 10,
-  },
-  {
-    name: "Principado de Asturias",
-    value: 5,
-  },
-  {
-    name: "Galicia",
-    value: 3,
+    value: 15,
   },
   {
     name: "Aragón",
-    value: 11,
+    value: 26,
   },
   {
-    name: "Castilla y León",
-    value: 19,
-  },
-  {
-    name: "Islas Canarias",
-    value: 18,
+    name: "Principado de Asturias",
+    value: 33,
   },
   {
     name: "Islas Baleares",
+    value: 17,
+  },
+  {
+    name: "Islas Canarias",
+    value: 12,
+  },
+  {
+    name: "Cantabria",
+    value: 23,
+  },
+  {
+    name: "Castilla-La Mancha",
+    value: 50,
+  },
+  {
+    name: "Castilla y León",
+    value: 47,
+  },
+  {
+    name: "Cataluña",
+    value: 42,
+  },
+  {
+    name: "Ceuta",
     value: 6,
   },
   {
-    name: "Comunidad Foral de Navarra",
-    value: 69,
+    name: "Comunidad Valenciana",
+    value: 21,
   },
-  {name: "Ceuta y Melilla",
-  value:  20,
+  {
+    name: "Extremadura",
+    value: 27,
+  },
+  {
+    name: "Galicia",
+    value: 21,
+  },
+  {
+    name: "Comunidad de Madrid",
+    value: 105,
+  },
+  { name: "Melilla", value: 27 },
+  {
+    name: "Región de Murcia",
+    value: 13,
+  },
+  {
+    name: "Comunidad Foral de Navarra",
+    value: 84,
+  },
+  {
+    name: "País Vasco",
+    value: 64,
+  },
+  {
+    name: "La Rioja",
+    value: 147,
   },
 ];
 
-export const covidAfter: resultado[] = [
+// IA 14 dias (02/11/2020) link datos:https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Actualizacion_241_COVID-19.pdf
+
+export const ola2: resultado[] = [
+  {
+    name: "Andalucía",
+    value: 545,
+  },
+  {
+    name: "Aragón",
+    value: 1127,
+  },
+  {
+    name: "Principado de Asturias",
+    value: 416,
+  },
+  {
+    name: "Islas Baleares",
+    value: 229,
+  },
+  {
+    name: "Islas Canarias",
+    value: 76,
+  },
+  {
+    name: "Cantabria",
+    value: 392,
+  },
+  {
+    name: "Castilla-La Mancha",
+    value: 518,
+  },
+  {
+    name: "Castilla y León",
+    value: 828,
+  },
+  {
+    name: "Cataluña",
+    value: 726,
+  },
+  {
+    name: "Ceuta",
+    value: 831,
+  },
+  {
+    name: "Comunidad Valenciana",
+    value: 225,
+  },
+  {
+    name: "Extremadura",
+    value: 556,
+  },
+  {
+    name: "Galicia",
+    value: 311,
+  },
+  {
+    name: "Comunidad de Madrid",
+    value: 403,
+  },
+  { name: "Melilla", value: 1356 },
+  {
+    name: "Región de Murcia",
+    value: 565,
+  },
+  {
+    name: "Comunidad Foral de Navarra",
+    value: 1193,
+  },
+  {
+    name: "País Vasco",
+    value: 640,
+  },
+  {
+    name: "La Rioja",
+    value: 772,
+  },
+];
+
+// IA 14 dias (20/01/2021) link datos: https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Actualizacion_294_COVID-19.pdf
+export const ola3: resultado[] = [
+  {
+    name: "Andalucía",
+    value: 676,
+  },
+  {
+    name: "Aragón",
+    value: 650,
+  },
+  {
+    name: "Principado de Asturias",
+    value: 351,
+  },
+  {
+    name: "Islas Baleares",
+    value: 673,
+  },
+  {
+    name: "Islas Canarias",
+    value: 180,
+  },
+  {
+    name: "Cantabria",
+    value: 360,
+  },
+  {
+    name: "Castilla-La Mancha",
+    value: 1066,
+  },
+  {
+    name: "Castilla y León",
+    value: 1047,
+  },
+  {
+    name: "Cataluña",
+    value: 615,
+  },
+  {
+    name: "Ceuta",
+    value: 429,
+  },
+  {
+    name: "Comunidad Valenciana",
+    value: 1075,
+  },
+  {
+    name: "Extremadura",
+    value: 1425,
+  },
+  {
+    name: "Galicia",
+    value: 542,
+  },
+  {
+    name: "Comunidad de Madrid",
+    value: 789,
+  },
+  { name: "Melilla", value: 698 },
+  {
+    name: "Región de Murcia",
+    value: 1189,
+  },
+  {
+    name: "Comunidad Foral de Navarra",
+    value: 412,
+  },
+  {
+    name: "País Vasco",
+    value: 373,
+  },
+  {
+    name: "La Rioja",
+    value: 1042,
+  },
+];
+
+// IA 14 dias (09/04/2021) link datos: https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Actualizacion_350_COVID-19.pdf
+export const covidNow: resultado[] = [
   {
     name: "Comunidad de Madrid",
     value: 324,
@@ -146,6 +306,5 @@ export const covidAfter: resultado[] = [
     name: "Comunidad Foral de Navarra",
     value: 395,
   },
-  {name: "Ceuta y Melilla",
-  value:  200},
+  { name: "Ceuta y Melilla", value: 200 },
 ];
--- Mandatory task. Module 10/src/index.ts
+++ Mandatory task. Module 10/src/index.ts
@@ -3,28 +3,38 @@ import * as topojson from "topojson-client";
 const spainjson = require("./spain.json");
 const d3Composite = require("d3-composite-projections");
 import { latLongCommunities } from "./communities";
-import { covidAfter, covidBefore, resultado } from "./covid";
+import { ola1, ola2, ola3, covidNow, resultado } from "./covid";
 
-// Numero maximo de contagiados entre todas las comunidades
-const maxAffected1 = covidAfter.reduce(
+const maxAffected1 = ola1.reduce(
+  (max, item) => (item.value > max ? item.value : max),
+  0
+);
+const maxAffected2 = ola2.reduce(
+  (max, item) => (item.value > max ? item.value : max),
+  0
+);
+const maxAffected3 = ola3.reduce(
+  (max, item) => (item.value > max ? item.value : max),
+  0
+);
+const maxAffected4 = covidNow.reduce(
   (max, item) => (item.value > max ? item.value : max),
   0
 );
 
-const maxAffected2 = covidBefore.reduce(
-  (max, item) => (item.value > max ? item.value : max),
-  0
+const maxAffected = Math.max(
+  maxAffected1,
+  maxAffected2,
+  maxAffected3,
+  maxAffected4
 );
 
-const maxAffected = Math.max(maxAffected1, maxAffected2);
-
-// Radio del circulo de cada comunidad en funcion del maximo de contagios
 const affectedRadiusScale = d3
   .scaleLinear()
   .domain([0, maxAffected])
   .range([0, 50]);
 
-let datos = covidAfter;
+let datos = ola1;
 
 const calculateRadiusBasedOnAffectedCases = (comunidad: string) => {
   const entry = datos.find((item) => item.name === comunidad);
@@ -85,257 +95,24 @@ const updateChart = (covid: resultado[]) => {
     });
 };
 
-updateChart(covidBefore);
+updateChart(ola1);
 
-document.getElementById("CovidBefore").addEventListener("click", function () {
-  console.log(covidBefore);
-  updateChart(covidBefore);
+document.getElementById("1wave").addEventListener("click", function () {
+  console.log(ola1);
+  updateChart(ola1);
 });
 
-document.getElementById("CovidAfter").addEventListener("click", function () {
-  console.log(covidAfter);
-  updateChart(covidAfter);
+document.getElementById("2wave").addEventListener("click", function () {
+  console.log(ola2);
+  updateChart(ola2);
 });
 
-/*
-import * as d3 from "d3";
-import * as topojson from "topojson-client";
-const spainjson = require("./spain.json");
-const d3Composite = require("d3-composite-projections");
-import { latLongCommunities } from "./communities";
-import {covidAfter, covidBefore, resultado} from "./covid";
+document.getElementById("3wave").addEventListener("click", function () {
+  console.log(ola3);
+  updateChart(ola3);
+});
 
-// Escala de colores para las comunidades segun la gravedad
-const color = d3
-  .scaleThreshold<number, string>()
-  .domain([0, 50, 100, 250, 500, 1000])
-  .range([
-    "#FFFFF",
-    "#FFE8E5",
-    "#F88F70",
-    "#CD6A4E",
-    "#A4472D",
-    "#7B240E",
-    "#540000",
-  ]);
-
-
-
-// Numero maximo de contagiados entre todas las comunidades
-const maxAffected1 = covidAfter.reduce(
-  (max,item) => (item.value > max ? item.value : max),0
-);
-
-const maxAffected2 = covidBefore.reduce(
-  (max,item) => (item.value > max ? item.value : max),0
-);
-
-const maxAffected = Math.max(maxAffected1, maxAffected2);
-
-
-// Radio del circulo de cada comunidad en funcion del maximo de contagios
-const affectedRadiusScale = d3
-  .scaleLinear()
-  .domain([0,maxAffected])
-  .range([0,50]);
-
-let datos = covidAfter;
-
-const calculateRadiusBasedOnAffectedCases = (comunidad : string) => {
-  const entry = datos.find(item => item.name === comunidad);
-
-  return entry ? affectedRadiusScale(entry.value) : 0;
-};
-
-
-const aProjection = d3Composite
-  .geoConicConformalSpain()
-  .scale(3300)
-  .translate([500, 400]);
-const geoPath = d3.geoPath().projection(aProjection);
-
-const geojson = topojson.feature(spainjson, spainjson.objects.ESP_adm1);
-
-const svg = d3
-  .select("body")
-  .append("svg")
-  .attr("width", 1024)
-  .attr("height", 800)
-  .attr("style", "background-color: #FBFAF0");
-
-
-const div = d3
-  .select("body")
-  .append("div")
-  .attr("class", "tooltip")
-  .style("opacity", 0);
-
-
-svg
-  .selectAll("path")
-  .data(geojson["features"])
-  .enter()
-  .append("path")
-  .attr("class", "country")
-  .attr("d", geoPath as any);
-
-
-svg
-  .selectAll("circle")
-  .data(latLongCommunities)
-  .enter()
-  .append("circle")
-  .attr("class", "affected-marker")
-  .attr("r", d => calculateRadiusBasedOnAffectedCases(d.name))
-  .attr("cx", (d) => aProjection([d.long, d.lat])[0])
-  .attr("cy", (d) => aProjection([d.long, d.lat])[1]);
-
-
-
-  const updateChart = (covid : resultado[]) => {
-    datos=covid;
-    svg
-      .selectAll("circle")
-      .data(latLongCommunities)
-      .enter()
-      .append("circle")
-      .attr("class", "affected-marker")
-      .attr("r", d => calculateRadiusBasedOnAffectedCases(d.name))
-      .attr("cx", (d) => aProjection([d.long, d.lat])[0])
-      .attr("cy", (d) => aProjection([d.long, d.lat])[1]);
-
-      const radio = d => calculateRadiusBasedOnAffectedCases(d.name);
-
-      const circ = d3
-        .arc()
-        .innerRadius(0)
-        .outerRadius(radio);
-  };
-  
-  document
-    .getElementById("CovidBefore")
-    .addEventListener("click", function() {
-      console.log(covidBefore);
-      updateChart(covidBefore);
-      //datos = CovidBefore;
-    });
-    
-  
-  document
-    .getElementById("CovidAfter")
-    .addEventListener("click", function() {
-      console.log(covidAfter)
-      updateChart(covidAfter);
-      //datos = CovidAfter;
-    });
-*/
-
-/*
-import * as d3 from "d3";
-import { on } from "node:events";
-import * as topojson from "topojson-client";
-const spainjson = require("./spain.json");
-const d3Composite = require("d3-composite-projections");
-import {latLongCommunities } from "./communities";
-import {CovidAfter, CovidBefore} from "./covid";
-
-
-const datos = CovidAfter;
-
-const color = d3
-  .scaleThreshold<number, string>()
-  .domain([0, 50, 100, 250, 500, 1000])
-  .range([
-    "#ffffae", 
-    "#f6de7e",  
-    "#efbc51", 
-    "#eaaa45", 
-    "#de8532",  
-    "#c35931",  
-    "#a13030"
-  ]);
-
-
-  const assignCommunitiesBackgroundColor = (comunidad: string) => {
-    const item = datos.find(
-      (item) => item.name === comunidad
-    );
-  
-    return item ? color(item.value) : color(0);
-  };
-  
-const aProjection = d3Composite
-  .geoConicConformalSpain()
-  .scale(3300)
-  .translate([500, 400]);
-const geoPath = d3.geoPath().projection(aProjection);
-
-const geojson = topojson.feature(spainjson, spainjson.objects.ESP_adm1);
-
-
-const svg = d3
-  .select("body")
-  .append("svg")
-  .attr("width", 1024)
-  .attr("height", 800)
-  .attr("style", "background-color: #FBFAF0");
-
-svg
-  .selectAll("path")
-  .data(geojson["features"])
-  .enter()
-  .append("path")
-  .attr("class", "country")
-  .attr("d", geoPath as any)
-  .style("fill",function(d : any) {
-    return assignCommunitiesBackgroundColor(d.properties.NAME_1)
-  });
-
-
-  // CIRCULOS DE GRAVEDAD
-  
-  const maxAffected = datos.reduce(
-    (max,item) => (item.value > max ? item.value : max),0
-  );
-
-  const affectedRadiusScale = d3
-  .scaleLinear()
-  .domain([0,maxAffected])
-  .range([0,50]);
-
-
-const calculateRadiusBasedOnAffectedCases = (comunidad : string ) => {
-  const entry = datos.find(item => item.name === comunidad);
-
-  return entry ? affectedRadiusScale(entry.value) : 0;
-};
-
-
-
-const div = d3
-  .select("body")
-  .append("div")
-  .attr("class", "tooltip")
-  .style("opacity", 0);
-
-
-svg
-  .selectAll("path")
-  .data(geojson["features"])
-  .enter()
-  .append("path")
-  .attr("class", "country")
-  .attr("d", geoPath as any);
-
-
-svg
-  .selectAll("circle")
-  .data(latLongCommunities)
-  .enter()
-  .append("circle")
-  .attr("class", "affected-marker")
-  .attr("r", d => calculateRadiusBasedOnAffectedCases(d.name)/2)
-  .attr("cx", (d) => aProjection([d.long, d.lat])[0])
-  .attr("cy", (d) => aProjection([d.long, d.lat])[1]);
-
-  */
+document.getElementById("now").addEventListener("click", function () {
+  console.log(covidNow);
+  updateChart(covidNow);
+});
